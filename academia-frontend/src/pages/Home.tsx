5c781eaa6f6d18961a5a5acd3533b8f1115b337d
RamilValiyev <ramil4360616@gmail.com>
2025-06-03 11:58:59 +0400

    Courses are added

--- academia-frontend/src/pages/Home.tsx
+++ academia-frontend/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import HeroSlider from "../components/HeroSlider";
+import Courses from "../pages/Courses";
 
 function Home() {
   return (
     <div>
       <HeroSlider />
+      <Courses />
     </div>
   );
 }
